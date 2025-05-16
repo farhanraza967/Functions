04d1a7ff32413577d2d585f89d5308ff00210c14
Farhan Raza <razafarhan967@gmail.com>
2025-05-16 22:05:14 +0500

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,19 +1,15 @@
 #include<iostream> 
 #include<iomanip> 
 using namespace std; 
-int factorial(int num) 
-{ 
+int factorial(int num){ 
     int counter, answer=1; 
-    if(num<0 || num>15) 
-    { 
+    if(num<0 || num>15){ 
         return 0;  
     } 
-    if(num==0) 
-    { 
+    if(num==0){ 
         return 1; 
     } 
-    for(counter=1;counter<=num;counter++) 
-    { 
+    for(counter=1;counter<=num;counter++){ 
         answer=answer*counter; 
     } 
     return answer; 
