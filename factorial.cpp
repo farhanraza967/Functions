ffb771c8cfa618c0dcc4a09949fdd0d9d68d5753
Farhan Raza <razafarhan967@gmail.com>
2025-05-16 22:07:28 +0500

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -2,25 +2,23 @@
 #include<iomanip> 
 using namespace std; 
 int factorial(int num){ 
-    int counter, answer=1; 
+    int ans=1; 
     if(num<0 || num>15){ 
         return 0;  
     } 
     if(num==0){ 
         return 1; 
     } 
-    for(counter=1;counter<=num;counter++){ 
-        answer=answer*counter; 
+    for(int counter=1; counter<=num; counter++){ 
+        ans=ans*counter; 
     } 
-    return answer; 
+    return ans; 
 }
-int main() 
-{ 
+int main(){ 
     int number, fact; 
     cout<<"Enter a positive number:"; 
     cin>>number; 
-    cout<<"\nFactorial of "<<number 
-        <<" is "<<factorial(number); 
+    cout<<"\nFactorial of "<<number << " is "<< factorial(number); 
     return 0; 
 } 
   
