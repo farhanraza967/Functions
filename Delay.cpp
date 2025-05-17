92ed60df54d3dd9306bc1471aab955a8e3ac0dac
Farhan Raza <razafarhan967@gmail.com>
2025-05-17 14:18:51 +0500

    Create Delay.cpp

---
+++ Delay.cpp
@@ -0,0 +1,26 @@
+#include<iostream> 
+#include<conio2.h> 
+using namespace std; 
+ 
+void delay(int num); 
+ 
+int main(void) 
+{ 
+    getch(); 
+    cout<<"Testing void function delay()\n"; 
+    delay(1); 
+    cout<<"This line is printed after delay value 1\n"; 
+    delay(2); 
+    cout<<"This line is printed after delay value 2\n"; 
+    delay(5); 
+    cout<<"This line is printed after delay value 5\n"; 
+    return 0; 
+} 
+ 
+void delay(int num) 
+{ 
+    for(int counter=1;counter<=num*100000000;counter++) 
+    { 
+ 
+    } 
+} 
