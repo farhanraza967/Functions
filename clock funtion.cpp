71ff332d2720c9d01ad0b4a161b084656e3bfb8d
Farhan Raza <razafarhan967@gmail.com>
2025-05-19 20:36:52 +0500

    Create clock funtion.cpp

---
+++ clock funtion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+void delayTwoSeconds() {
+    clock_t start = clock();
+    clock_t delay = 2 * CLOCKS_PER_SEC;
+    while (clock() - start < delay) {
+
+    }
+}
+int main(){
+    cout << "Wait for 2 seconds..." << std::endl;
+    delayTwoSeconds();
+    cout << "Done!" << std::endl;
+    return 0;
+}
